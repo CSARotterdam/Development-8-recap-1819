fd687942b5d92d760a78bda33ab1fc2c935a4e91
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2019-07-04 23:29:13 +0200

    Added solutions

---
+++ Dev8Recap/Oplossingen_livecoding.fs
@@ -0,0 +1,44 @@
+﻿module Oplossingen_livecoding
+open Validation
+
+let rec mapList (func : 'a -> 'b) (l : List<'a>) : List<'b> =
+    match l with
+    | [] -> []
+    | head :: tail -> func head :: (mapList func tail)
+
+let test1 = ValidateMapList mapList
+
+
+let rec append (list1 : List<'a>) (list2:List<'a>) : List<'a> =
+    match list1 with
+    | head :: tail -> head :: (append tail list2)
+    | [] ->
+        match list2 with
+        | head :: tail -> head :: (append [] tail)
+        | [] -> []
+
+let test2 = ValidateAppend append
+
+
+let rec last (l : List<'a>) : Option<'a> =
+    match l with
+    | [] -> None
+    | head :: tail -> 
+        match tail with
+        | [] -> Some head
+        | _ -> last tail
+
+let test3 = ValidateLast last
+
+
+let rec zip (list1 : List<'a>) (list2 : List<'b>) : Option<List<'a*'b>> =
+    match (list1, list2) with
+    | ([],[]) -> Some []
+    | (head1::tail1,head2::tail2) ->
+        let tailresult = zip tail1 tail2
+        match tailresult with
+        | Some goedelijst -> (head1,head2) :: goedelijst |> Some
+        | None -> None
+    | (_,_) -> None
+
+let test4 = ValidateZip zip
